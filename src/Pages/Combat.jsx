c8a31234ea98e77d005d69b9f4f348201b5df634
JonPeterP <55144487+JonPeterP@users.noreply.github.com>
2023-04-03 12:19:18 +0800

    Add create combat button function

--- src/Pages/Combat.jsx
+++ src/Pages/Combat.jsx
@@ -1,19 +1,43 @@
 import { Button } from "bootstrap";
 import React from "react";
-import {Roll} from '../App';
+import { Roll } from '../App';
 
-function Combat(){
-    return(
-    <div>
-        <p>Test</p>
-        <button onClick={() =>{
-            Roll("passing text");
-          
-        }}>Roll 20</button>
-    </div>
+function Combat() {
+
+
+    let btns = [];
+    for (let i = 0; i < 5; i++) {
+        btns.push(<BtnCombat key={i} btnname={i + " long name very long name"} clickMessage={"pressed " + i.toString()} />)
+    }
+    return (
+        <div className="divCombat">
+        <h3>Weapons</h3>
+            <div className="divWeaponBtns">
+                {btns}
+            </div>
+        </div>
+
     )
 }
 
 
+function BtnCombat({ key, btnname, clickMessage }) {
+    return (
+        <div class = "divLblBtnCombat">        
+        <h4 class="lblCombat">{btnname}</h4>
+        <button id={key} className="btnCombat" onClick={() => {
+            Roll(clickMessage);
+        }}>
+            {"To Hit " + "[+5]"}
+        </button>
+        <button id={key} className="btnCombat" onClick={() => {
+            Roll(clickMessage);
+        }}>
+            {"Damage " + "1d5+5"}
+        </button>
+        </div>
+
+    )
+}
 
 export default Combat;
